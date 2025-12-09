0660f184a5166d824ad18a903cb0a3c64664994e
logicalPanda2 <TBA>
2025-12-09 13:42:26 +0700

    Add type guard

--- recipe-finder-api/src/App.tsx
+++ recipe-finder-api/src/App.tsx
@@ -5,64 +5,172 @@ export default function App() {
 	const [page, setPage] = useState<number>(1);
 	const [searchValue, setSearchValue] = useState<string>("");
 	const [recipes, setRecipes] = useState<Recipe[]>([]);
-    const [isRecipeActive, setIsRecipeActive] = useState<boolean>(false);
-    const [activeRecipe, setActiveRecipe] = useState<unknown>({});
+	const [isRecipeActive, setIsRecipeActive] = useState<boolean>(false);
+	const [activeRecipe, setActiveRecipe] = useState<unknown>({});
 
-    interface Recipe {
-        idMeal: string,
-        strMeal: string,
-        strMealAlternate: unknown,
-        strCategory: string,
-        strArea: string,
-        strInstructions: string,
-        strMealThumb: string,
-        strTags: string,
-        strYoutube: string,
-        strIngredient1: string,
-        strIngredient2: string,
-        strIngredient3: string,
-        strIngredient4: string,
-        strIngredient5: string,
-        strIngredient6: string,
-        strIngredient7: string,
-        strIngredient8: string,
-        strIngredient9: string,
-        strIngredient10: string,
-        strIngredient11: string,
-        strIngredient12: string,
-        strIngredient13: string,
-        strIngredient14: string,
-        strIngredient15: string,
-        strIngredient16: string,
-        strIngredient17: string,
-        strIngredient18: string,
-        strIngredient19: string,
-        strIngredient20: string,
-        strMeasure1: string,
-        strMeasure2: string,
-        strMeasure3: string,
-        strMeasure4: string,
-        strMeasure5: string,
-        strMeasure6: string,
-        strMeasure7: string,
-        strMeasure8: string,
-        strMeasure9: string,
-        strMeasure10: string,
-        strMeasure11: string,
-        strMeasure12: string,
-        strMeasure13: string,
-        strMeasure14: string,
-        strMeasure15: string,
-        strMeasure16: string,
-        strMeasure17: string,
-        strMeasure18: string,
-        strMeasure19: string,
-        strMeasure20: string,
-        strSource: string,
-        strImageSource: unknown,
-        strCreativeCommonsConfirmed: unknown,
-        dateModified: unknown
-    }
+	interface Recipe {
+		idMeal: string;
+		strMeal: string;
+		strMealAlternate: unknown;
+		strCategory: string;
+		strArea: string;
+		strInstructions: string;
+		strMealThumb: string;
+		strTags: string;
+		strYoutube: string;
+		strIngredient1: string;
+		strIngredient2: string;
+		strIngredient3: string;
+		strIngredient4: string;
+		strIngredient5: string;
+		strIngredient6: string;
+		strIngredient7: string;
+		strIngredient8: string;
+		strIngredient9: string;
+		strIngredient10: string;
+		strIngredient11: string;
+		strIngredient12: string;
+		strIngredient13: string;
+		strIngredient14: string;
+		strIngredient15: string;
+		strIngredient16: string;
+		strIngredient17: string;
+		strIngredient18: string;
+		strIngredient19: string;
+		strIngredient20: string;
+		strMeasure1: string;
+		strMeasure2: string;
+		strMeasure3: string;
+		strMeasure4: string;
+		strMeasure5: string;
+		strMeasure6: string;
+		strMeasure7: string;
+		strMeasure8: string;
+		strMeasure9: string;
+		strMeasure10: string;
+		strMeasure11: string;
+		strMeasure12: string;
+		strMeasure13: string;
+		strMeasure14: string;
+		strMeasure15: string;
+		strMeasure16: string;
+		strMeasure17: string;
+		strMeasure18: string;
+		strMeasure19: string;
+		strMeasure20: string;
+		strSource: string;
+		strImageSource: unknown;
+		strCreativeCommonsConfirmed: unknown;
+		dateModified: unknown;
+	}
+
+	function isRecipe(value: unknown): value is Recipe {
+		if (
+			typeof value === "object" &&
+			value !== null &&
+			"idMeal" in value &&
+			typeof value.idMeal === "string" &&
+			"strMeal" in value &&
+			typeof value.strMeal === "string" &&
+			"strCategory" in value &&
+			typeof value.strCategory === "string" &&
+			"strArea" in value &&
+			typeof value.strArea === "string" &&
+			"strInstructions" in value &&
+			typeof value.strInstructions === "string" &&
+			"strMealThumb" in value &&
+			typeof value.strMealThumb === "string" &&
+			"strTags" in value &&
+			typeof value.strTags === "string" &&
+			"strYoutube" in value &&
+			typeof value.strYoutube === "string" &&
+			"strIngredient1" in value &&
+			typeof value.strIngredient1 === "string" &&
+			"strIngredient2" in value &&
+			typeof value.strIngredient2 === "string" &&
+			"strIngredient3" in value &&
+			typeof value.strIngredient3 === "string" &&
+			"strIngredient4" in value &&
+			typeof value.strIngredient4 === "string" &&
+			"strIngredient5" in value &&
+			typeof value.strIngredient5 === "string" &&
+			"strIngredient6" in value &&
+			typeof value.strIngredient6 === "string" &&
+			"strIngredient7" in value &&
+			typeof value.strIngredient7 === "string" &&
+			"strIngredient8" in value &&
+			typeof value.strIngredient8 === "string" &&
+			"strIngredient9" in value &&
+			typeof value.strIngredient9 === "string" &&
+			"strIngredient10" in value &&
+			typeof value.strIngredient10 === "string" &&
+			"strIngredient11" in value &&
+			typeof value.strIngredient11 === "string" &&
+			"strIngredient12" in value &&
+			typeof value.strIngredient12 === "string" &&
+			"strIngredient13" in value &&
+			typeof value.strIngredient13 === "string" &&
+			"strIngredient14" in value &&
+			typeof value.strIngredient14 === "string" &&
+			"strIngredient15" in value &&
+			typeof value.strIngredient15 === "string" &&
+			"strIngredient16" in value &&
+			typeof value.strIngredient16 === "string" &&
+			"strIngredient17" in value &&
+			typeof value.strIngredient17 === "string" &&
+			"strIngredient18" in value &&
+			typeof value.strIngredient18 === "string" &&
+			"strIngredient19" in value &&
+			typeof value.strIngredient19 === "string" &&
+			"strIngredient20" in value &&
+			typeof value.strIngredient20 === "string" &&
+			"strMeasure1" in value &&
+			typeof value.strMeasure1 === "string" &&
+			"strMeasure2" in value &&
+			typeof value.strMeasure2 === "string" &&
+			"strMeasure3" in value &&
+			typeof value.strMeasure3 === "string" &&
+			"strMeasure4" in value &&
+			typeof value.strMeasure4 === "string" &&
+			"strMeasure5" in value &&
+			typeof value.strMeasure5 === "string" &&
+			"strMeasure6" in value &&
+			typeof value.strMeasure6 === "string" &&
+			"strMeasure7" in value &&
+			typeof value.strMeasure7 === "string" &&
+			"strMeasure8" in value &&
+			typeof value.strMeasure8 === "string" &&
+			"strMeasure9" in value &&
+			typeof value.strMeasure9 === "string" &&
+			"strMeasure10" in value &&
+			typeof value.strMeasure10 === "string" &&
+			"strMeasure11" in value &&
+			typeof value.strMeasure11 === "string" &&
+			"strMeasure12" in value &&
+			typeof value.strMeasure12 === "string" &&
+			"strMeasure13" in value &&
+			typeof value.strMeasure13 === "string" &&
+			"strMeasure14" in value &&
+			typeof value.strMeasure14 === "string" &&
+			"strMeasure15" in value &&
+			typeof value.strMeasure15 === "string" &&
+			"strMeasure16" in value &&
+			typeof value.strMeasure16 === "string" &&
+			"strMeasure17" in value &&
+			typeof value.strMeasure17 === "string" &&
+			"strMeasure18" in value &&
+			typeof value.strMeasure18 === "string" &&
+			"strMeasure19" in value &&
+			typeof value.strMeasure19 === "string" &&
+			"strMeasure20" in value &&
+			typeof value.strMeasure20 === "string" &&
+			"strSource" in value &&
+			typeof value.strSource === "string"
+		) {
+			return true;
+		}
+		return false;
+	}
 
 	const recipesPerPage = 3;
 	const start = (page - 1) * recipesPerPage;
@@ -79,16 +187,16 @@ export default function App() {
 		}
 	};
 
-    const openDialogBox = (r: unknown) => {
-        setIsRecipeActive(true);
-        setActiveRecipe(r);
-    }
+	const openDialogBox = (r: unknown) => {
+		setIsRecipeActive(true);
+		setActiveRecipe(r);
+	};
 
-    const closeDialogBox = () => {
-        setIsRecipeActive(false);
-        console.log(activeRecipe);
-        setActiveRecipe({});
-    }
+	const closeDialogBox = () => {
+		setIsRecipeActive(false);
+		console.log(activeRecipe);
+		setActiveRecipe({});
+	};
 
 	const handlePrevPage = () => {
 		if (page > 1) {
@@ -151,18 +259,26 @@ export default function App() {
 				/>
 			</header>
 			<main>
-				{
-					visibleRecipes.length !== 0 ? (
-						visibleRecipes.map((r: Recipe) => (
-							<div key={r.idMeal} onClick={() => {openDialogBox(r)}}>
-                                <p>{r.strMeal}</p>
-                                <img src={r.strMealThumb} alt="" width={100} height={100}/>
-                            </div>
-						))
-					) : (
-						<p>No results found</p>
-					)
-				}
+				{visibleRecipes.length !== 0 ? (
+					visibleRecipes.map((r: Recipe) => (
+						<div
+							key={r.idMeal}
+							onClick={() => {
+								openDialogBox(r);
+							}}
+						>
+							<p>{r.strMeal}</p>
+							<img
+								src={r.strMealThumb}
+								alt=""
+								width={100}
+								height={100}
+							/>
+						</div>
+					))
+				) : (
+					<p>No results found</p>
+				)}
 				<button
 					onClick={handlePrevPage}
 					className="border border-solid border-blue-500"
@@ -176,7 +292,11 @@ export default function App() {
 				>
 					Next
 				</button>
-                {isRecipeActive && <div onClick={closeDialogBox}>this will be the dialog box that displays recipe</div>}
+				{isRecipeActive && (
+					<div onClick={closeDialogBox}>
+						this will be the dialog box that displays recipe
+					</div>
+				)}
 			</main>
 			<footer>
 				API provided by (link to themealdb) design by (link to github

a708650b81a51f579f75756de587ed6574d76c8f
Marcel <marcelrg.business@gmail.com>
2025-12-10 18:12:45 +0700

    Add style

--- recipe-finder-api/src/components/Pagination.tsx
+++ recipe-finder-api/src/components/Pagination.tsx
@@ -6,17 +6,19 @@ interface Props {
 
 export default function Pagination({ page, onPrevious, onNext }: Props) {
 	return (
-		<div>
+		<div 
+            className="flex grow flex-row justify-center items-center gap-4 sm:gap-8 sm:pt-12 pt-4"
+        >
 			<button
 				onClick={onPrevious}
-				className="border border-solid border-blue-500"
+				className="border border-solid border-black rounded-lg w-24 py-2"
 			>
 				Previous
 			</button>
 			<p>{page}</p>
 			<button
 				onClick={onNext}
-				className="border border-solid border-blue-500"
+				className="border border-solid border-black rounded-lg w-24 py-2"
 			>
 				Next
 			</button>

0a6de87d81636b5c72943a22af75fbce443e9235
logicalPanda2 <TBA>
2025-12-10 17:04:30 +0700

    Format

--- recipe-finder-api/src/components/RecipeThumbnail.tsx
+++ recipe-finder-api/src/components/RecipeThumbnail.tsx
@@ -2,27 +2,23 @@ import { useEffect } from "react";
 import type { Recipe } from "../interface/Recipe";
 
 interface Props {
-    recipe: Recipe,
-    onOpen: (recipe: unknown) => void,
+	recipe: Recipe;
+	onOpen: (recipe: unknown) => void;
 }
 
-export default function RecipeThumbnail({recipe, onOpen}: Props) {
-    useEffect(() => {
-        console.log(recipe.idMeal);
-    })
+export default function RecipeThumbnail({ recipe, onOpen }: Props) {
+	useEffect(() => {
+		console.log(recipe.idMeal);
+	});
 
-    return (
-    <div
-        onClick={() => {
-            onOpen(recipe);
-        }}
-    >
-        <p>{recipe.strMeal}</p>
-        <img
-            src={recipe.strMealThumb}
-            alt=""
-            width={100}
-            height={100}
-        />
-    </div>);
+	return (
+		<div
+			onClick={() => {
+				onOpen(recipe);
+			}}
+		>
+			<p>{recipe.strMeal}</p>
+			<img src={recipe.strMealThumb} alt="" width={100} height={100} />
+		</div>
+	);
 }

15cf73e182a0e9a54a592cf69222b639b6ce1327
logicalPanda2 <TBA>
2025-12-10 17:41:14 +0700

    Add style

--- recipe-finder-api/src/components/Header.tsx
+++ recipe-finder-api/src/components/Header.tsx
@@ -5,19 +5,33 @@ interface Props {
 
 export default function Header({ value, onChange }: Props) {
 	return (
-		<header>
-			<h1>Recipe Searcher</h1>
-			<label htmlFor="searchField">Search</label>
-			<input
-				type="search"
-				name="query"
-				id="searchField"
-				className="border border-solid border-black"
-				value={value}
-				onChange={(e) => {
-					onChange(e.target.value);
-				}}
-			/>
+		<header
+            className="flex flex-col flex-nowrap p-4 items-center"
+        >
+			<h1
+                className="text-4xl text-center font-semibold mb-4"
+            >
+                Recipe Searcher
+            </h1>
+            <div
+                className="flex flex-col items-center justify-center px-4 w-1/3 min-w-3xs"
+            >
+                <label htmlFor="searchField"
+                    className="mb-1 text-xl font-semibold"
+                >
+                    Search
+                </label>
+                <input
+                    type="search"
+                    name="query"
+                    id="searchField"
+                    className="border-[1.5px] border-solid border-black rounded-lg px-2 py-1 w-[90%]"
+                    value={value}
+                    onChange={(e) => {
+                        onChange(e.target.value);
+                    }}
+                />
+            </div>
 		</header>
 	);
 }

65e783c733989c4170d494eef7b3a7883f9abd22
logicalPanda2 <TBA>
2025-12-13 16:23:09 +0700

    Improve UX

--- recipe-finder-api/src/components/RecipeThumbnail.tsx
+++ recipe-finder-api/src/components/RecipeThumbnail.tsx
@@ -11,7 +11,7 @@ export default function RecipeThumbnail({ recipe, onOpen }: Props) {
 			onClick={() => {
 				onOpen(recipe);
 			}}
-            className="flex flex-col items-center bg-gray-100 [box-shadow:0_1px_2px_rgba(0,0,0,0.5)] rounded-lg p-4 w-2/3 min-w-40 sm:w-1/6 sm:min-w-48 gap-2 hover:bg-gray-50/90 focus-visible:bg-gray-50/90 focus-visible:[box-shadow:0_2px_4px_rgba(0,0,0,0.75)] focus-visible:outline-0 transition"
+            className="flex flex-col items-center bg-gray-100 [box-shadow:0_1px_2px_rgba(0,0,0,0.5)] rounded-lg p-4 w-2/3 min-w-40 sm:w-1/6 sm:min-w-48 gap-2 hover:bg-gray-50/90 focus-visible:bg-gray-50/90 focus-visible:[box-shadow:0_2px_4px_rgba(0,0,0,0.75)] focus-visible:outline-0 transition cursor-pointer"
 		>
 			<p
                 className="text-xl"

0df109902d5122aa1d706d88c39cf627bab9e531
Marcel <marcelrg.business@gmail.com>
2025-12-20 15:16:17 +0700

    Implement caching

--- recipe-finder-api/src/main.tsx
+++ recipe-finder-api/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -9,8 +10,21 @@ if (!rootElement) {
 	throw new Error("Root element not found");
 }
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60 * 5,
+            gcTime: 1000 * 60 * 30,
+            refetchOnWindowFocus: false,
+            retry: 1,
+        }
+    }
+});
+
 createRoot(rootElement).render(
 	<StrictMode>
-		<App />
+        <QueryClientProvider client={queryClient}>
+		    <App />
+        </QueryClientProvider>
 	</StrictMode>,
 );
